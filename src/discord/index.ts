ac6413d75f624b074f2b3de73b09a1cf464b4f6d
dworac <mail@dworac.com>
2023-01-05 11:09:49 -0600

    feat: added discord logs

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -3,12 +3,15 @@ import intents from './config/intents';
 import onRun from './events/onReady';
 import onInteraction from './events/onInteraction';
 import config from '../utils/config';
+import Logger from '../utils/logger';
 
 export default async () => {
 	const client = new Client({ intents });
 
+	Logger.logInfo('Starting Discord client...');
+
 	client.on('ready', async () => {
-		await onRun(client);
+		await onRun();
 	});
 
 	client.on('interactionCreate', async (interaction) => {
